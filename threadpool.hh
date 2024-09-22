5bd08e857e34cd17bfabe822450c93b96282fc9d
jmr <1515629148@qq.com>
2024-09-22 17:06:49 +0800

    write threadpool class

---
+++ threadpool.hh
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <vector>
+#include <queue>
+#include <memory>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+
+// 任务抽象基类
+class Task {
+public:
+    // 用户可以自定义任意任务类型，从Task继承，重写run方法, 实现自定义任务处理
+    virtual void run() = 0;
+private:
+};
+
+// 线程池支持的模式
+enum class PoolMode {
+    MODE_FIXED, // 固定数量的线程
+    MODE_CACHED,// 线程数量可动态增长
+};
+
+// 线程类型
+class Thread {
+public:
+private:
+};
+
+// 线程池类型
+class ThreadPool {
+public:
+    ThreadPool();
+    ~ThreadPool();
+
+    // 设置线程池的工作模式
+    void setMode(PoolMode mode);
+    // 开启线程池
+    void start();
+
+private:
+    std::vector<Thread*> thread_; // 线程列表
+    std::size_t init_thread_size_;     // 初始的线程数量
+    // 防止用户传递临时对象，用智能指针延长生命周期
+    std::queue<std::shared_ptr<Task>> task_que_; // 任务队列
+    std::atomic_uint task_size_;    // 任务数量
+    int taskque_max_threshhold_;    // 任务队列数量上限阈值
+
+    std::mutex taskque_mutx_;   // 保证任务队列的线程安全
+    std::condition_variable not_full_;  // 表示任务队列不满
+    std::condition_variable not_empty_; // 表示任务队列不空
+
+    PoolMode pool_mode_;    // 当前线程池的工作模式
+};
